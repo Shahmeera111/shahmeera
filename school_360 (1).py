dbda6b72bb936d1c95d5b70c1e1d85ea7a3c9e90
AhmedMu14 <Ahmedmujahid2004@gmail.com>
2026-06-28 01:24:39 +0500

    fix: path name

--- school_360 (1).py
+++ school_360 (1).py
@@ -568,7 +568,7 @@ Instructions:
 """)
 
 st.image(
-    "/content/ishihara_plate.ppm", # Corrected filename based on previous upload output
+    "/ishihara_plate.ppm", # Corrected filename based on previous upload output
     width=600
 )
 
